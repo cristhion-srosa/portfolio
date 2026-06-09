6e3ad39f9f7c5c1f0f47fd82a737a8e3d391b4f1
cristhion rosa <silvacristhion@gmail.com>
2026-06-09 00:59:27 -0300

    feat(stack): promote technical expertise

--- src/modules/portfolio/components/AboutSection.vue
+++ src/modules/portfolio/components/AboutSection.vue
@@ -8,17 +8,32 @@ const { t } = useI18n();
 <template>
   <section id="about" class="section about">
     <SectionHeading :index="t.about.index" :title="t.about.title" />
+    <div class="stack-showcase">
+      <header class="stack-showcase__header">
+        <p class="eyebrow">{{ t.about.stackLabel }}</p>
+        <p>{{ t.about.stackIntro }}</p>
+      </header>
+      <div class="stack-groups">
+        <article
+          v-for="(group, index) in t.about.stack"
+          :key="group.label"
+          class="stack-group"
+        >
+          <header>
+            <span>0{{ index + 1 }}</span>
+            <h3>{{ group.label }}</h3>
+          </header>
+          <ul>
+            <li v-for="item in group.items" :key="item">{{ item }}</li>
+          </ul>
+        </article>
+      </div>
+    </div>
     <div class="about__grid">
       <div class="about__copy">
         <p v-for="paragraph in t.about.body" :key="paragraph">{{ paragraph }}</p>
       </div>
       <div class="about__details">
-        <div>
-          <p class="eyebrow">{{ t.about.stackLabel }}</p>
-          <ul class="stack-list">
-            <li v-for="item in t.about.stack" :key="item">{{ item }}</li>
-          </ul>
-        </div>
         <div>
           <p class="eyebrow">{{ t.about.educationLabel }}</p>
           <p>{{ t.about.education }}</p>
